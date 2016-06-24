c24a8fbd34e387b1fb19ca07898829e2709e54ff
Sai Gatram <gatramsai@gmail.com>
2016-06-24 16:00:01 -0500

    Modified the display message activity to print the message entered into the text box. Completed. To do: add a button to go back to the text box.

--- MyFirstApp/app/src/main/java/saigatram/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/app/src/main/java/saigatram/myfirstapp/DisplayMessageActivity.java
@@ -1,7 +1,10 @@
 package saigatram.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
     }
+
+
+
+
 }
