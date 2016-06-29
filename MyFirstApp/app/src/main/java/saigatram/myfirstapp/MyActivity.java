2f105925c40e241ae0e7b7febc1d008d7052481f
Sai Gatram <gatramsai@gmail.com>
2016-06-29 16:14:16 -0500

    Added long and lat details. And added Coarse Location into app.

--- MyFirstApp/app/src/main/java/saigatram/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/saigatram/myfirstapp/MyActivity.java
@@ -3,18 +3,31 @@ package saigatram.myfirstapp;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class MyActivity extends AppCompatActivity {
+
+public class MyActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener
+{
     public final static String EXTRA_MESSAGE = "saigatram.myfirstapp.MESSAGE";
     private static final int MY_PERMISSIONS_REQUEST_GET_LOCATION = 1;
+    private GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    TextView mLatitudeText,mLongitudeText;
 
 
     //The following code checks if the app has permission to read the user's contacts, and requests the permission if necessary
@@ -22,9 +35,20 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        //check if permssion is gotten
+
+        /*check if permssion is gotten
         int permissionCheck = ContextCompat.checkSelfPermission(MyActivity.this,
                 Manifest.permission.ACCESS_COARSE_LOCATION);
+        */
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
         if (ContextCompat.checkSelfPermission(MyActivity.this,
                 Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -49,7 +73,11 @@ public class MyActivity extends AppCompatActivity {
                 // result of the request.
             }
         }
+
+        mLatitudeText=(TextView)findViewById(R.id.textView1);
+        mLongitudeText=(TextView)findViewById(R.id.textView2);
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -82,8 +110,37 @@ public class MyActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
 
-    //The following code checks if the app has permission to read the user's contacts, and requests the permission if necessary
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
 
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        int permissionCheck = ContextCompat.checkSelfPermission(MyActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
 
